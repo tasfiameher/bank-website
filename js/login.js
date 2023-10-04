862535cfd1963c57f39a2d3b21360a84cd80c336
tasfiameher <tasfiameher060@gmail.com>
2023-10-04 23:04:43 +0600

    index file added

---
+++ js/login.js
@@ -0,0 +1,21 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+    //    step-2: get the email address inside the email input field
+    //   always remember to use .value to get text from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    //    step-3: get password
+    //  3.a: get id on the html element
+    // 3.b: get the element
+    //  3.c:  get the value from the element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email, password);
+    // DANGER: DO NOT VERIFY email pass on the client side
+    // step-4: verify email and password and check valid user or not
+    if (email === 'sontain@gmail.com' && password === 'secret') {
+        window.location.href = 'bank.html';
+    }
+    else {
+        alert('invalid user');
+    }
+})
